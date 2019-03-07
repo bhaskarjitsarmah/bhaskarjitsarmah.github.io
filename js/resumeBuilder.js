d1d062939b999abe6d2e9116f83504c50796b4b1
bhaskarjitsarmah <bhaskarjitsarmah@gmail.com>
2019-03-08 01:17:51 +0530

    javascript changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,188 +1,193 @@
-var work = {
-	"jobs":[
-		{
-		"employer": "WNS Global Services"
-		"title": "Data Scientist"
-		"location": "Gurgaon"
-		}, 
-		
-		{
-		"employer": "Capgemini"
-		"title": "Software Engineer"
-		"location": "Mumbai"
-		}
-	], 
-
-	//Display work json object in resume object
-	display: function() {
-		for (job in work.jobs) {
-			$("#workExperience").append(HTMLworkStart);
-			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-			var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-			var formettedEmployerTitle = formattedEmployer + formattedTitle;
-			$(".work-entry:last").append(formettedEmployerTitle);
-
-		}
-
-	},
-
-	//Find all location that I have worked
-	locationizer: function(_work) {
-		var allLocation = [];
-		for (var work in _work.jobs) {
-			var _workLoc = _work.jobs[work].location;
-			allLocation.push(_workLoc);
-		}
-		return allLocation;
-
-	}
-}
-
-//////////////////////////////////////////////////BIO/////////////////////////////////////////////////////////////////
 var bio = {
-	"name": "Sahel Mastoureshgh",
-	"role": "Software Engineer",
-	"contacts": {
-		"mobile": "323-895-0521",
-		"email": "sahel.mastoureshgh@gmail.com",
-		"github": "www.github.com/sahelmastoureshgh",
-		"location": "Louisville,KY"
-	},
-	"welcomeMessage": "Welcome to my resume website",
-	"skills": [
-		"programming", "web development", "JS", "MongoDB", "Java"
-	],
-	"bioPic": "images/sahelsmall.png",
+  name: 'Bhaskarjit Sarmah',
+  role: 'Data Scientist',
+  contacts: {
+    mobile: '9022087053',
+    email: 'bhaskarjit(at)gmail',
+    github: 'bsarmah',
+    twitter: 'bsarmah',
+    location: 'Delhi, India'
+  },
+  welcomeMessage: 'I am a newly graduate full stack engineer currently working as a mentor at Udacity and Google Summer of Code.',
+  skills: ['Golang', 'Backend', 'Python', 'Flask', 'React', 'Docker'],
+  biopic: 'images/bhaskarjit.jpeg'
+};
 
-	//Dispay bio json object in resume page
-	display: function() {
-		var formattedName = HTMLheaderName.replace("%data%", bio.name);
-		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-		var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-		$("#topContacts").append(formattedMobile);
-		$("#footerContacts").append(formattedMobile);
-		var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-		$("#topContacts").append(formattedEmail);
-		$("#footerContacts").append(formattedEmail);
-		var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-		$("#topContacts").append(formattedGithub);
-		$("#footerContacts").append(formattedGithub);
-		var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-		$("#topContacts").append(formattedLocation);
-		$("#footerContacts").append(formattedLocation);
-		var formattedWeclome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
-		$("#header").append(formattedWeclome);
-		var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-		$("#header").append(formattedBioPic);
-		if (bio.skills.length > 0) {
-			$("#header").append(HTMLskillsStart);
-			for (var i = 0; i < bio.skills.length; i++) {
-				var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-				$("#skills").append(formattedSkill);
-			}
-		}
-		$("#header").prepend(formattedRole);
-		$("#header").prepend(formattedName);
-
-	}
-}
-//////////////////////////////////////////////////PROJET/////////////////////////////////////////////////////////////////
-var projects = {
-	"projects": [{
-		"title": "News Website",
-		"dates": "November 2013",
-		"description": "Implemented a website which gathers the latest news feeds from xml format of different news agencies and has the ability to share news in Facebook. Using Node.js in server side running in Amazon EC2 cloud platform. For client side used Backbone.js, HTML5, Twitter bootstrap framework.",
-		"images": ['http://i.imgur.com/PhC5STL.png']
-
-	}],
-	//Display projects in resume page
-	display: function() {
-		for (project in projects.projects) {
-			$("#projects").append(HTMLprojectStart);
-			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-			$(".project-entry:last").append(formattedTitle);
-			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-			$(".project-entry:last").append(formattedDates);
-			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-			$(".project-entry:last").append(formattedDescription);
-			if (projects.projects[project].images.length > 0) {
-				for (image in projects.projects[project].images) {
-					var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-					$(".project-entry:last").append(formattedImages);
-				}
-			}
-
-		}
-
-	}
-}
-//////////////////////////////////////////////////EDUCATION/////////////////////////////////////////////////////////////////
 var education = {
-	"schools": [ {
-		"name": "Worcester Polytechnic Institute",
-		"location": "Worcester, MA",
-		"degree": "Master",
-		"majors": ["ComputerScience"],
-		"dates": "August 2010 - May 2012"
+  schools: [{
+    name: 'IIIT Vadodara',
+    location: 'Vadodara',
+    degree: 'B.Tech',
+    majors: ['Computer Science'],
+    dates: '2014-18'
+  }, ],
+  onlineCourses: [{
+      title: 'Full Stack ND',
+      school: 'Udacity',
+      dates: 'July 2017',
+      url: 'https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004'
+    },
+    {
+      title: 'Android ND',
+      school: 'Udacity',
+      dates: '2016',
+      url: 'https://www.udacity.com/course/android-developer-nanodegree-by-google--nd801'
+    }
+  ]
+};
 
-	}],
-	"onlineCourses": [{
-		"title": "Tackling the Challenges of Big Data",
-		"school": "MIT",
-		"dates": "February 2014",
-		"url": "https://mitprofessionalx.edx.org/courses/MITProfessionalX/6.BD_2X/3T2015/about"
-	}, {
-		"title": "Data Wrangling with MongoDB",
-		"school": "Udacity",
-		"dates": "May 2014",
-		"url": "https://www.udacity.com/course/ud032"
+var work = {
+  jobs: [{
+      employer: 'Appbase.io',
+      title: 'Infrastructure Engineer',
+      location: 'Delhi',
+      dates: '2017',
+      description: 'Worked on the Go backend infrastructure of Appbase.io'
+    },
+    {
+      employer: 'Udacity',
+      title: 'Mentor',
+      location: 'Gandhinagar',
+      dates: '2017',
+      description: 'Mentored students on full stack and Udacity courses.'
+    },
+    {
+      employer: 'Google Summer of Code',
+      title: 'Software Developer',
+      location: 'Patna',
+      dates: '2016',
+      description: 'Worked on a Flask backend server and Docker deployment.'
+    },
+  ]
+};
 
-	}, {
-		"title": "MongoDB for Node.js Developers",
-		"school": "MongoDB",
-		"dates": "July 2014",
-		"url": "https://university.mongodb.com/courses/M101JS/about"
-	}],
-	//Display education in my resume page
-	display: function() {
-		for (edu in education.schools) {
-			$('#education').append(HTMLschoolStart);
-			var formattedName = HTMLschoolName.replace("%data%", education.schools[edu].name);
-			var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[edu].degree);
-			var formattedNameAndDegree = formattedName + formattedDegree;
-			$(".education-entry:last").append(formattedNameAndDegree);
-			var formattedDate = HTMLschoolDates.replace("%data%", education.schools[edu].dates);
-			$(".education-entry:last").append(formattedDate);
-			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[edu].location);
-			$(".education-entry:last").append(formattedLocation);
-			if (education.schools[edu].majors.length > 0) {
-				for (major in education.schools[edu].majors) {
-					var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[edu].majors[major]);
-					$(".education-entry:last").append(formattedMajor);
-				}
-			}
+var projects = {
+  projects: [{
+      title: 'Clipjump',
+      dates: '2013-16',
+      description: 'Clipboard manager for Windows with 100k downloads.',
+      images: ['images/clipjump_1.jpg', 'images/clipjump_2.jpg'],
+      url: 'https://is.gd/clipjump'
+    },
+    {
+      title: 'Portfolio',
+      dates: '2017',
+      description: 'Dynamic portfolio built in React',
+      url: 'http://aviaryan.in/work'
+    }
+  ]
+};
 
-		}
-		//online classes
-		$('#education').append(HTMLonlineClasses)
-		for (online in education.onlineCourses) {
-			$('#education').append(HTMLschoolStart);
-			var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
-			var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
-			var formattedTitleAndSchool = formattedTitle + formattedSchool;
-			$(".education-entry:last").append(formattedTitleAndSchool);
-			var formattedDate = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
-			$(".education-entry:last").append(formattedDate);
-			var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[online].url);
-			$(".education-entry:last").append(formattedURL);
 
-		}
+/*
+ * Display functions
+ */
+bio.display = function() {
+  // basic details
+  var name = HTMLheaderName.replace('%data%', bio.name);
+  var role = HTMLheaderRole.replace('%data%', bio.role);
+  var bioPic = HTMLbioPic.replace('%data%', bio.biopic);
+  var message = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+  // contacts
+  var contact = [
+    HTMLmobile.replace('%data%', bio.contacts.mobile),
+    HTMLemail.replace('%data%', bio.contacts.email),
+    HTMLgithub.replace('%data%', bio.contacts.github),
+    HTMLtwitter.replace('%data%', bio.contacts.twitter),
+    HTMLlocation.replace('%data%', bio.contacts.location)
+  ];
 
-	}
+  $('#header').prepend(role);
+  $('#header').prepend(name);
+  $('#header').append(bioPic);
+  $('#header').append(message);
+
+  contact.forEach(function(c) {
+    $('#topContacts').append(c);
+    $('#footerContacts').append(c);
+  });
+
+  if (bio.skills.length > 0) {
+    $('#header').append(HTMLskillsStart);
+    bio.skills.forEach(function(s) {
+      $('#skills').append(HTMLskills.replace('%data%', s));
+    });
+  }
+};
+
+education.display = function() {
+  // schools
+  if (education.schools.length > 0) {
+    education.schools.forEach(function(s) {
+      $('#education').append(HTMLschoolStart);
+      var name = HTMLschoolName.replace('%data%', s.name);
+      var degree = HTMLschoolDegree.replace('%data%', s.degree);
+      var dates = HTMLschoolDates.replace('%data%', s.dates);
+      var location = HTMLschoolLocation.replace('%data%', s.location);
+      var major = HTMLschoolMajor.replace('%data%', s.majors[0]);
+
+      $('.education-entry:last').append(name + degree, [dates, location, major]);
+    });
+  }
+  // online courses
+  if (education.onlineCourses.length > 0) {
+    $('#education').append(HTMLonlineClasses);
+    education.onlineCourses.forEach(function(oc) {
+      $('#education').append(HTMLschoolStart);
+      var title = HTMLonlineTitle.replace('%data%', oc.title).replace('#', oc.url);
+      var school = HTMLonlineSchool.replace('%data%', oc.school);
+      var dates = HTMLonlineDates.replace('%data%', oc.dates);
+      var url = HTMLonlineURL.replace('%data%', oc.url).replace('#', oc.url);
+
+      $('.education-entry:last').append(title + school, [dates, url]);
+    });
+  }
+};
+
+work.display = function() {
+  if (work.jobs.length > 0) {
+    $('#workExperience').append(HTMLworkStart);
+    work.jobs.forEach(function(job) {
+      var employer = HTMLworkEmployer.replace('%data%', job.employer);
+      var title = HTMLworkTitle.replace('%data%', job.title);
+      var location = HTMLworkLocation.replace('%data%', job.location);
+      var worked = HTMLworkDates.replace('%data%', job.dates);
+      var description = HTMLworkDescription.replace('%data%', job.description);
+      title = employer + title;
+
+      $('.work-entry:last').append(title, [location, worked, description]);
+    });
+  }
+};
+
+projects.display = function() {
+  if (projects.projects.length > 0) {
+    projects.projects.forEach(function(pj) {
+      $('#projects').append(HTMLprojectStart);
+      var title = HTMLprojectTitle.replace('%data%', pj.title).replace('#', pj.url);
+      var dates = HTMLprojectDates.replace('%data%', pj.dates);
+      var description = HTMLprojectDescription.replace('%data%', pj.description);
+
+      $('.project-entry:last').append(title, [dates, description]);
+      // IMAGES
+      if (pj.images) {
+        pj.images.forEach(function(img) {
+          var image = HTMLprojectImage.replace('%data%', img);
+          $('.project-entry:last').append(image);
+        });
+      }
+    });
+  }
+};
+
+function setupMap() {
+  $('#mapDiv').append(map);
 }
 
-work.display();
+// display stuff
 bio.display();
-projects.display();
 education.display();
-$("#mapDiv").append(googleMap);
+work.display();
+projects.display();
+$('#mapDiv').append(googleMap);
+window.addEventListener('load', setupMap);
